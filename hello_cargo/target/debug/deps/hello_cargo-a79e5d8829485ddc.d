d1d84f9e44708bd84634cdd4f72ef030a85caf5d
dr4g0n7ly <nishanth.pilli@gmail.com>
2023-05-19 22:55:06 +0530

    Build

---
+++ hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d
@@ -0,0 +1,5 @@
+/home/unix/github/Rust/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc: src/main.rs
+
+/home/unix/github/Rust/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/unix/github/Rust/hello_cargo/target/debug/hello_cargo: /home/unix/github/Rust/hello_cargo/src/main.rs
