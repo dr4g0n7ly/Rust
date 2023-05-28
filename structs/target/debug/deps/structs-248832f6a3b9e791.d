42c827834ee0ca34be467549184171222ce556c4
dr4g0n7ly <nishanth.pilli@gmail.com>
2023-05-28 16:25:51 +0530

    changes

---
+++ structs/target/debug/deps/structs-248832f6a3b9e791.d
@@ -0,0 +1,5 @@
+/home/unix/github/Rust/structs/target/debug/deps/structs-248832f6a3b9e791: src/main.rs
+
+/home/unix/github/Rust/structs/target/debug/deps/structs-248832f6a3b9e791.d: src/main.rs
+
+src/main.rs:
---
+++ structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+/home/unix/github/Rust/structs/target/debug/structs: /home/unix/github/Rust/structs/src/main.rs
