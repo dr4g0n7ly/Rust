5ab374ee281f5e0c30fc3faca72bdd3b03e22c8c
dr4g0n7ly <nishanth.pilli@gmail.com>
2023-05-31 00:08:58 +0530

    input

--- employee_hashmap/src/main.rs
+++ employee_hashmap/src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 use std::collections::HashMap;
 
 #[derive(Debug)]
@@ -15,6 +16,32 @@ fn main() {
     add_employee("Eng".to_string(), "Nishanth".to_string(), &mut emp_count, &mut employees);
     add_employee("Dsg".to_string(), "PRaks".to_string(), &mut emp_count, &mut employees);
     
+    loop {
+        println!("\nAdd employee: (1)\nQuit: (other)");
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read line");
+
+        if input == String::from("1") {
+            let mut dept = String::new();
+            let mut name = String::new();
+
+            println("Enter department: ");
+            io::stdin()
+                .read_line(&mut dept)
+                .expect("failed to read line");
+
+            println("Enter name: ");
+            io::stdin()
+                .read_line(&mut name)
+                .expect("failed to read line");
+
+            add_employee(dept, name, emp_count, employees);
+        }
+    }
+
     println!("total employees: {emp_count}");
     
     for (dept, emp_vec) in &employees {
