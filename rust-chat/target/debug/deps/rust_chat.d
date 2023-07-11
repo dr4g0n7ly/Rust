3ac5855ca00f836a3a5b6c2c1a01cdd30b7cd31d
dr4g0n7ly <nishanth.pilli@gmail.com>
2023-07-11 14:34:09 +0530

    rust chat frontend

--- rust-chat/target/debug/deps/rust_chat.d
+++ rust-chat/target/debug/deps/rust_chat.d
@@ -3,3 +3,5 @@ C:\Users\nisha\Documents\GitHub\Rust\rust-chat\target\debug\deps\rust_chat.exe: 
 C:\Users\nisha\Documents\GitHub\Rust\rust-chat\target\debug\deps\rust_chat.d: src\main.rs
 
 src\main.rs:
+
+# env-dep:CARGO_MANIFEST_DIR=C:\\Users\\nisha\\Documents\\GitHub\\Rust\\rust-chat
