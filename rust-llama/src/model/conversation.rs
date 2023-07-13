73a691e23f99dda9bb0e669579037a125d12976f
dr4g0n7ly <nishanth.pilli@gmail.com>
2023-07-13 21:48:17 +0530

    added serde

--- rust-llama/src/model/conversation.rs
+++ rust-llama/src/model/conversation.rs
@@ -1,11 +1,21 @@
 use serde::Deserialize;
 use serde::Serialize;
 
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Message {
     pub text: String,
     pub user: bool,
 }
 
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Conversation {
     pub messages: Vec<Message>
+}
+
+impl Conversation {
+    pub fn new() -> Conversation {
+        Conversation { 
+            messages: Vec::new() 
+        }
+    }
 }
